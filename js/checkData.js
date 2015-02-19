705f5dce11abc1ea004a4e47c7591ffae3b6e091
MythiliM <mmythili@gmail.com>
2015-02-19 15:22:00 -0800

    Image event handlers for checks cashed

--- js/checkData.js
+++ js/checkData.js
@@ -3,7 +3,14 @@ var checks = [
 	{"checkNumber":1002, "amount":120.13, "date": "2/17/2015", "photo":"../images/check.jpg"}
 ];
 
+//Show the flyout
+function showChecksFlyout(eventInfo) {
+    var image = document.getElementById(eventInfo.detail.itemIndex);
+    document.getElementById("checkFlyout").winControl.show(image);
+}
+
 WinJS.Namespace.define("CheckData.ListView", {
-    data: new WinJS.Binding.List(checks)
+    data: new WinJS.Binding.List(checks),    
+    showChecksFlyout: showChecksFlyout
 });
 WinJS.UI.processAll();
