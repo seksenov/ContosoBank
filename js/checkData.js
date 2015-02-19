04d02eb759aad9a344371900852b6b7eb09c9f0e
Kevin Hill <kevinhill@live.com>
2015-02-19 14:27:24 -0800

    Got list view working

--- js/checkData.js
+++ js/checkData.js
@@ -1,6 +1,6 @@
 var checks = [
-	{"checkNumber":1001, "amount":20.54, "date": "2/16/2015", "photo":"..\images\check.jpg"},
-	{"checkNumber":1002, "amount":120.13, "date": "2/17/2015", "photo":"..\images\check.jpg"}
+	{"checkNumber":1001, "amount":20.54, "date": "2/16/2015", "photo":""},
+	{"checkNumber":1002, "amount":120.13, "date": "2/17/2015", "photo":""}
 ];
 
 WinJS.Namespace.define("CheckData.ListView", {
