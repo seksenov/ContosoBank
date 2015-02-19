d9d74b1f67556404a07c8d215e1fe825a6ea460d
MythiliM <mmythili@gmail.com>
2015-02-19 14:17:44 -0800

    Updates to checks cashed

--- js/checkData.js
+++ js/checkData.js
@@ -2,3 +2,8 @@ var checks = [
 	{"checkNumber":1001, "amount":20.54, "date": "2/16/2015", "photo":"..\images\check.jpg"},
 	{"checkNumber":1002, "amount":120.13, "date": "2/17/2015", "photo":"..\images\check.jpg"}
 ];
+
+WinJS.Namespace.define("CheckData.ListView", {
+    data: new WinJS.Binding.List(checks)
+});
+WinJS.UI.processAll();
