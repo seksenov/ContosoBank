ad8f2fcc1f9c302f3d59ee66a2a01fd2a26ab116
Alex Lu <alex@example.com>
2015-02-19 14:33:20 -0800

    Fragment loading test

--- js/default.js
+++ js/default.js
@@ -1,1 +1,22 @@
-WinJS.UI.processAll();
+WinJS.UI.processAll().done(function() {
+
+
+function loadFragment(fragmentDir, fragmentDiv) {
+
+  WinJS.UI.Fragments.renderCopy(fragmentDir, fragmentDiv)
+    .done(
+        function() {
+          console.log("successfully loaded fragment");
+        },
+        function(error) {
+          console.log(error);
+        }
+    );
+
+}
+
+var checksCashedFragment = document.getElementById('checksCashedFragment');
+
+loadFragment('/pages/ChecksCashed.html', checksCashedFragment);
+
+});
